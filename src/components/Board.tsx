139deebf7e28f82216616f4d7e39953590c1d9c2
richcire <eric9907@naver.com>
2022-08-21 16:04:25 +0900

    add motion to label in board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,21 +3,36 @@ import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { memoStorageState } from "../atoms";
 
-const BoardContainer = styled(motion.ul)`
+const BoardContainer = styled.ul`
   background-color: aliceblue;
   width: 30%;
 `;
-const Label = styled.li``;
+const Label = styled(motion.li)``;
 
 interface IBoard {
   boardType: "todo" | "finished" | "willdo";
 }
 function Board({ boardType }: IBoard) {
   const [memoStorage, setMemoStorage] = useRecoilState(memoStorageState);
+
+  const finishedMoveRight = (offsetMoved: number, memoId: string) => {
+    console.log(memoStorage.finished.findIndex((memo) => memo?.id === memoId));
+  };
+  const todoMovedLeft = () => {};
+  const todoMovedRight = () => {};
+  const willdoMmoveLeft = () => {};
   return (
-    <BoardContainer drag whileDrag={{ scale: 1.2 }}>
+    <BoardContainer>
       {memoStorage[boardType].map((memo) => (
-        <Label>{memo?.text}</Label>
+        <Label
+          key={memo?.id}
+          drag="x"
+          whileDrag={{ scale: 1.2 }}
+          dragConstraints={{ left: 0, right: 0 }}
+          onDrag={(event, info) => finishedMoveRight(info.offset.x, memo?.id!)}
+        >
+          {memo?.text}
+        </Label>
       ))}
     </BoardContainer>
   );
